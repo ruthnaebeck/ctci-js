f02b12197fa1b07dc043424dc96451e2a9df5dce
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-05-29 16:54:13 -0400

    Added sort solution

--- src/chapter1/ch1-q1.js
+++ src/chapter1/ch1-q1.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Use an object O(n)
 export function isUnique1(str){
   var chars = {};
   for(var i = 0; i < str.length; i++){
@@ -9,12 +10,22 @@ export function isUnique1(str){
   return true;
 }
 
+// Use nested for loops O(n2)
 export function isUnique2(str){
   for(var i = 0; i < str.length; i++){
     for(var j = 0; j < str.length; j++){
       if(i === j) break;
       if(str[i] === str[j]) return false;
     }
+  }
+  return true;
+}
+
+// Could also use sort
+export function isUnique3(str){
+  var arr = str.split('').sort();
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i] === arr[i+1]) return false;
   }
   return true;
 }
