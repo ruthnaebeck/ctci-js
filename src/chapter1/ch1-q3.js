a964141ee9ef39959dafd5e6a6521e61c580ccdc
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-05-29 20:11:10 -0400

    Solved ch1-q3

--- src/chapter1/ch1-q3.js
+++ src/chapter1/ch1-q3.js
@@ -1,5 +1,10 @@
 'use strict';
 
-export function first(){
-  //
+export function urlify(str){
+  if(!str || !str.length) return str;
+  var arr = str.split('');
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i] === ' ') arr[i] = '%20';
+  }
+  return arr.join('');
 }
