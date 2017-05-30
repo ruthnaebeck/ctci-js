1c09384d4f7ced0d53f1419623dd6837cc298b85
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-05-30 15:15:11 -0400

    Solved ch1-q5

--- src/chapter1/ch1-q5.js
+++ src/chapter1/ch1-q5.js
@@ -1,5 +1,55 @@
 'use strict';
 
-export function first(){
-  //
+// Separated Insert / Remove and Replace
+export function oneAway1(str1, str2){
+  if(str1 === str2) return true;
+  if(Math.abs(str1.length - str2.length) > 1) return false;
+  var arr1 = str1.split('');
+  var arr2 = str2.split('');
+  var num = Math.max(arr1.length, arr2.length);
+  var diff = 0;
+  if(arr1.length - arr2.length){
+    // Insert / Remove operation
+    for(let i = 0; i < num; i++){
+      if(arr1[i] !== arr2[i]){
+        diff++;
+        if(arr1.length > arr2.length) arr1.splice(i, 1);
+        else arr2.splice(i, 1);
+      }
+      if(diff > 1) return false;
+    }
+  }else{
+    // Replace operation
+    for(let i = 0; i < num; i++){
+      if(arr1[i] !== arr2[i]) diff++;
+      if(diff > 1) return false;
+    }
+  }
+  return true;
+}
+
+// Combined Insert / Remove and Replace
+export function oneAway2(str1, str2){
+  if(str1 === str2) return true;
+  if(Math.abs(str1.length - str2.length) > 1) return false;
+  var arr1 = str1.split('');
+  var arr2 = str2.split('');
+  var num = Math.max(arr1.length, arr2.length);
+  var diff = 0;
+  var len = arr1.length - arr2.length;
+  for(var i = 0; i < num; i++){
+    if(len){
+      // Insert / Remove operation
+      if(arr1[i] !== arr2[i]){
+        diff++;
+        if(len > 0) arr1.splice(i, 1);
+        else arr2.splice(i, 1);
+      }
+    }else{
+      // Replace operation
+      if(arr1[i] !== arr2[i]) diff++;
+    }
+    if(diff > 1) return false;
+  }
+  return true;
 }
