c724b7b99e5d38567454d4b44513a72b94d48e00
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-05-29 20:56:47 -0400

    Solved ch1-q4

--- src/chapter1/ch1-q4.js
+++ src/chapter1/ch1-q4.js
@@ -1,5 +1,19 @@
 'use strict';
 
-export function first(){
-  //
+export function permOfPalindrome(str){
+  if(!str) return false;
+  var obj = {};
+  for(var i = 0; i < str.length; i++){
+    let char = str[i];
+    if(char !== ' '){
+      let val = obj[char];
+      if(val) obj[char]++;
+      else obj[char] = 1;
+    }
+  }
+  var count = 0;
+  for(var key in obj){
+    if(obj[key] % 2 !== 0) count++;
+  }
+  return count < 2;
 }
