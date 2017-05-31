d6800eb277c12b7696801a27bc2b9fa06e2d5120
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-05-30 22:12:45 -0400

    Solved ch1-q6

--- src/chapter1/ch1-q6.js
+++ src/chapter1/ch1-q6.js
@@ -1,5 +1,18 @@
 'use strict';
 
-export function first(){
-  //
+export function compress(str){
+  if(!str) return str;
+  var num = 1;
+  var char = str[0];
+  var result = '';
+  for(var i = 1; i <= str.length; i++){
+    if(str[i] === char){
+      num++;
+    }else{
+      result += char + num;
+      num = 1;
+      char = str[i];
+    }
+  }
+  return result.length < str.length ? result : str;
 }
