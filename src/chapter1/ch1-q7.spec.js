56b0b6edf4b66c34c4e32df831ffba4488261fbc
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-06-01 11:28:21 -0400

    Change spec to accept any error

--- src/chapter1/ch1-q7.spec.js
+++ src/chapter1/ch1-q7.spec.js
@@ -13,9 +13,9 @@ for (let key in funcs) {
   describe('ch1-q7: ' + key, function() {
 
     it('throws an error when null/undefined/empty matrix', function() {
-      expect(() => func(null)).to.throw('invalid matrix');
-      expect(() => func(undefined)).to.throw('invalid matrix');
-      expect(() => func([])).to.throw('invalid matrix');
+      expect(() => func(null)).to.throw(Error);
+      expect(() => func(undefined)).to.throw(Error);
+      expect(() => func([])).to.throw(Error);
     });
 
     it('returns matrix unchanged with 1,1 matrix', function() {
