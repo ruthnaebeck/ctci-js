93fb573013f8aa63350ece6ad5fe2ae10eff46df
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-06-01 19:36:06 -0400

    Solved ch1-q7

--- src/chapter1/ch1-q7.js
+++ src/chapter1/ch1-q7.js
@@ -1,5 +1,31 @@
 'use strict';
 
-export function first(){
-  //
+// Book solution slightly modified to return error or matrix
+
+// Path for a 4 by 4 matrix
+// 1st Iteration of 1st for loop:
+  // 2nd for loop:
+    // #1 - Change the 4 corners
+    // #2 - Change #'s adjacent to 4 corners (R,U,L,D)
+    // #3 - Change #'s adjacent to #'s in previous step
+// 2nd Iteration of 1st for loop:
+  // 2nd for loop:
+    // #1 - Change the inner 4 corners
+
+export function rotateMatrix(matrix){
+  if(!matrix || !matrix.length) throw Error('Error');
+  var len = matrix.length;
+  for(var i = 0; i < len / 2; i++){
+    let first = i;
+    let last = len - 1 - i;
+    for(var j = first; j < last; j++){
+      let offset = j - first;
+      let top = matrix[first][j]; // save top
+      matrix[first][j] = matrix[last-offset][first]; // left -> top
+      matrix[last-offset][first] = matrix[last][last-offset]; // bottom -> left
+      matrix[last][last-offset] = matrix[j][last]; // right -> bottom
+      matrix[j][last] = top; // top -> right
+    }
+  }
+  return matrix;
 }
