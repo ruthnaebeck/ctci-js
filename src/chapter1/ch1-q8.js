4166fd3674dcb859e8641e951bb0d07804feffe6
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-06-02 14:24:46 -0400

    Solved ch1-q8

--- src/chapter1/ch1-q8.js
+++ src/chapter1/ch1-q8.js
@@ -1,5 +1,26 @@
 'use strict';
 
-export function first(){
-  //
+// Loops over the matrix twice
+export function zeroMatrix1(matrix){
+  if(!matrix) throw Error('Error');
+  var rows = [];
+  var cols = [];
+  // Collects the zeros
+  matrix.forEach((row, rIdx) =>{
+    row.forEach((item, cIdx) =>{
+      if(!item){
+        rows.push(rIdx);
+        cols.push(cIdx);
+      }
+    });
+  });
+  // Changes vales to zeros
+  matrix.forEach((row, rIdx) =>{
+    row.forEach((item, cIdx) =>{
+      if(rows.indexOf(rIdx) > -1 || cols.indexOf(cIdx) > -1){
+        matrix[rIdx][cIdx] = 0;
+      }
+    });
+  });
+  return matrix;
 }
