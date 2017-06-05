ade01f76dda21238db74b57c4e0babb71e072830
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-06-05 15:14:15 -0400

    Solved ch2-q2

--- src/chapter2/ch2-q2.js
+++ src/chapter2/ch2-q2.js
@@ -1,5 +1,21 @@
 'use strict';
 
-export function first(){
-  //
+// Solution counts the nodes
+// then finds the kth to the last node
+export function kToLast1(list, k){
+  if(!list) throw Error('Error');
+  var count = 0;
+  var cNode = list;
+  while(cNode.next){
+    count++;
+    cNode = cNode.next;
+  }
+  if(count < k) throw Error('Error');
+  var x = count - k;
+  cNode = list;
+  while(x){
+    cNode = cNode.next;
+    x--;
+  }
+  return cNode.val;
 }
