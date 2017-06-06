2e4b70ba227caaa78bbdcb8370b77c2b0e0a0c11
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-06-06 14:38:13 -0400

    Solved ch2-q3

--- src/chapter2/ch2-q3.js
+++ src/chapter2/ch2-q3.js
@@ -1,5 +1,9 @@
 'use strict';
 
-export function first(){
-  //
+// NOTE - List is not given
+// EASY - Just replace given node with next
+export function deleteNode(node){
+  if(!node || !node.next) throw Error('Error');
+  node.val = node.next.val;
+  node.next = node.next.next;
 }
