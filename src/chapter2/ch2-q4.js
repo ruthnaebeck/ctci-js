3e65e8493bd69449aba772afa9157e07e364beef
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-06-08 17:18:26 -0400

    Solved ch2-q4

--- src/chapter2/ch2-q4.js
+++ src/chapter2/ch2-q4.js
@@ -1,5 +1,26 @@
 'use strict';
 
-export function first(){
-  //
+// Book solution working with 2 lists
+export function partition(list, x){
+  var smallList = null;
+  var smallNode = null;
+  var othList = null;
+  var cNode = list;
+  while(cNode){
+    if(cNode.val < x){
+      smallList = new ListNode(cNode.val, smallList);
+      if(!smallNode) smallNode = smallList;
+    }else{
+      othList = new ListNode(cNode.val, othList);
+    }
+    cNode = cNode.next;
+  }
+  if(!smallNode) return list;
+  smallNode.next = othList;
+  return smallList;
+}
+
+function ListNode(value, next){
+  this.val = value;
+  this.next = next || null;
 }
