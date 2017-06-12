ae6f21408a7fc2de308eefc86e5cc93b8add46cb
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-06-12 19:43:44 -0400

    Solved ch2-q8

--- src/chapter2/ch2-q8.js
+++ src/chapter2/ch2-q8.js
@@ -1,5 +1,16 @@
 'use strict';
 
-export function first(){
-  //
+export function detectLoop(list){
+  var nodes = [];
+  var cNode = list;
+  while(cNode){
+    for(var i = 0; i < nodes.length; i++){
+      if(cNode === nodes[i]){
+        return cNode;
+      }
+    }
+    nodes.push(cNode);
+    cNode = cNode.next;
+  }
+  return null;
 }
