bf5f9d14a4d39127ab45c3b0467d30d692a6ac6b
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-06-11 20:39:14 -0400

    Solved ch2-q7

--- src/chapter2/ch2-q7.js
+++ src/chapter2/ch2-q7.js
@@ -1,5 +1,15 @@
 'use strict';
 
-export function first(){
-  //
+// Loop over both lists to find the intersecting node
+// Time O(m x n)
+export function intersection(list1, list2){
+  var cNode1 = list1;
+  while(cNode1){
+    let cNode2 = list2;
+    while(cNode2){
+      if(cNode1 === cNode2) return cNode1;
+      cNode2 = cNode2.next;
+    }
+    cNode1 = cNode1.next;
+  }
 }
